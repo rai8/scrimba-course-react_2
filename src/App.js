14371f60bcd96bf51f9f7eb1d4d06b323e0d746e
rai8 <hytonne@outlook.com>
2019-08-27 12:37:03 +0300

    componentDidMOunt and fetch API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
-  );
+//fetch method allows us perfom http request so that we can get any data we want
+//read on promises
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      character: {}
+    };
+  }
+  componentDidMount() {
+    this.setState({ loading: true });
+    fetch("https://swapi.co/api/people/1")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          loading: false,
+          character: data
+        });
+      });
+  }
+
+  render() {
+    const text = this.state.loading
+      ? "loading... kindly wait"
+      : this.state.character.name;
+    return (
+      <div>
+        <h1> Hello world</h1>
+        <p>{text}</p>
+      </div>
+    );
+  }
 }
-
 export default App;
