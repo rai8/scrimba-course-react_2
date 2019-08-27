55906185292e5132148ac6fc8cbbbb0414b2683a
rai8 <hytonne@outlook.com>
2019-08-27 15:33:51 +0300

    handling input type text

--- src/components/Form_practice.js
+++ src/components/Form_practice.js
@@ -6,30 +6,53 @@ export default class Form_practice extends Component {
     this.state = {
       firstName: "",
       lastName: "",
-      age: 0,
+      age: "",
       gender: "",
       destination: "",
       dietaryRestriction: []
     };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    //pulling elements from event.target
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
   }
 
   render() {
     return (
       <div>
         <form>
-          <input placeholder="First Name" />
+          <input
+            name="firstName"
+            value={this.state.firstName}
+            placeholder="First Name"
+            onChange={this.handleChange}
+          />
           <br />
-          <input placeholder="Last Name" />
+          <input
+            name="lastName"
+            value={this.state.lastName}
+            placeholder="Last Name"
+            onChange={this.handleChange}
+          />
           <br />
-          <input placeholder="Age" />
+          <input
+            name="age"
+            value={this.state.age}
+            placeholder="Age"
+            onChange={this.handleChange}
+          />
           <br />
           <button>Sumbit</button>
         </form>
         <hr />
         <h2>Entered information :</h2>
-        <p>Your Name: </p>
+        <p>
+          Your Name: {this.state.firstName} {this.state.lastName}{" "}
+        </p>
         <p>Your Gender: </p>
-        <p>Your Age: </p>
+        <p>Your Age: {this.state.age}</p>
         <p>Your Destination: </p>
         <p>Your Dietary restrictions: </p>
       </div>
