5f6539cf19fc24a4404f576fa17e75cc0551809e
rai8 <hytonne@outlook.com>
2019-08-27 13:10:50 +0300

    on change event function

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,14 +3,26 @@ import React, { Component } from "react";
 export default class Form extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      firstName: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ firstName: e.target.value });
   }
   render() {
     return (
       <div>
         <h1>fill the form below</h1>
         <form>
-          <input type="text" placeholder="First Name" />
+          <input
+            type="text"
+            placeholder="First Name"
+            onChange={this.handleChange}
+          />
+          <p>{this.state.firstName}</p>
         </form>
       </div>
     );
